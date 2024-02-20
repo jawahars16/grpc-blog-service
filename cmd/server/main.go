df977259f933ecf2f8a73853c3cb6ba78e50faf5
Jawahar <jawahar.selvaraj@onefootball.com>
2024-02-20 20:39:12 +0100

    created grpc Server to serve blog functionalities

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/jawahars16/grpc-blog-service/internal/post"
+	"google.golang.org/grpc"
+)
+
 func main() {
+	port := 9000
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatal("Sever failed to start\n", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	post.RegisterBlogServer(grpcServer, post.NewBlogServer())
+
+	log.Printf("Server started on port %d\n", port)
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatal("gRPC server failed to start\n", err)
+	}
 }
