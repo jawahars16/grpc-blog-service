7e0dc774dd452ae3ca2b2680e1d607f73b0a4a0c
Jawahar <jawahar.selvaraj@onefootball.com>
2024-02-20 22:26:01 +0100

    added read loop to read user input continously for client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/jawahars16/grpc-blog-service/internal/post"
 	"google.golang.org/grpc"
@@ -21,16 +24,81 @@ func main() {
 	}
 	defer conn.Close()
 
-	fmt.Println("Connected to server")
+	loop(context.Background(), post.NewBlogClient(conn))
+}
 
-	client := post.NewBlogClient(conn)
-	res, err := client.CreatePost(context.Background(), &post.CreatePostRequest{
-		Title:   "Test Title",
-		Content: "Test Content",
-	})
-	if err != nil {
-		fmt.Println("Failed to create post.", err)
-		return
+func loop(ctx context.Context, client post.BlogClient) {
+	for {
+		var input string
+		fmt.Println("")
+		fmt.Println("Enter command to interact with blogging platform. Type 'exit' to quit.")
+		fmt.Println("[Commands: create, get, list, delete, exit]")
+		fmt.Print("> ")
+		fmt.Scanln(&input)
+
+		switch input {
+		case "exit":
+			fmt.Println("Exiting...")
+			return
+		case "create":
+			postDetails := readPostDetails()
+			response, err := client.CreatePost(ctx, &post.CreatePostRequest{
+				Title:   postDetails.Title,
+				Content: postDetails.Content,
+				Author:  postDetails.Author,
+				Tags:    postDetails.Tags,
+			})
+			if err != nil {
+				fmt.Println("Failed to create post.", err)
+				continue
+			}
+			fmt.Println("Post created successfully.", response.Post)
+			continue
+		case "get":
+			var input string
+			fmt.Print("Post ID: ")
+			fmt.Scanln(&input)
+			response, err := client.GetPost(ctx, &post.GetPostRequest{
+				PostId: input,
+			})
+			if err != nil {
+				fmt.Println("Error fetching post.", err)
+			}
+			fmt.Println(response.Post)
+			continue
+		default:
+			fmt.Println("Invalid input.")
+			continue
+		}
+
+		fmt.Println("You entered:", input)
+		fmt.Println("")
 	}
-	fmt.Println("Post created successfully.", res.Post)
+}
+
+func readPostDetails() *post.Post {
+	scanner := bufio.NewScanner(os.Stdin)
+	post := &post.Post{}
+	fmt.Print("Title: ")
+	if scanner.Scan() {
+		post.Title = scanner.Text()
+	}
+
+	fmt.Print("Content: ")
+	if scanner.Scan() {
+		post.Content = scanner.Text()
+	}
+
+	fmt.Print("Author: ")
+	if scanner.Scan() {
+		post.Author = scanner.Text()
+	}
+
+	fmt.Print("Tags (comma separated): ")
+	if scanner.Scan() {
+		post.Tags = strings.Split(scanner.Text(), ",")
+	}
+
+	fmt.Println("")
+	return post
 }
